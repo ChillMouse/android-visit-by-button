565a7655acdf1a925c2a400afdac4e02892cddf9
chillmouse <koftun251201@inbox.ru>
2021-12-05 19:54:35 +0700

    Click on button and visit second layout with image

---
+++ app/src/main/java/ru/kovtunenko/pr_21_kovtunenko/SecondActivity.java
@@ -0,0 +1,18 @@
+package ru.kovtunenko.pr_21_kovtunenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
